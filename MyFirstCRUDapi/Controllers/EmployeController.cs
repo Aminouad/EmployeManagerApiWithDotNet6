6baacedd30998227d389bd0fa6bec325b6d5255a
Amine Ouadrani <ouadrani12@gmail.com>
2022-02-18 03:24:10 +0100

    complete the controller with all his crud methods

--- MyFirstCRUDapi/Controllers/EmployeController.cs
+++ MyFirstCRUDapi/Controllers/EmployeController.cs
@@ -14,6 +14,13 @@ namespace MyFirstCRUDapi.Controllers
                     FirstName="jordan",
                     LastName="ok",
                     place="UK"
+                },
+                new employe {
+                    id = 2,
+                    name ="john keny",
+                    FirstName="john",
+                    LastName="keny",
+                    place="US"
                 }
             };
         [HttpGet]
@@ -22,10 +29,47 @@ namespace MyFirstCRUDapi.Controllers
             
             return Ok(employes);
         }
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<employe>> Get(int id)
+        {
+            var employe = employes.Find(e=> e.id==id);
+            if(employe == null)
+            {
+                return BadRequest("employee not found");
+            }
+            return Ok(employe);
+        }
         [HttpPost]
         public async Task<ActionResult<List<employe>>> AddEmployee(employe emp)
         {
             employes.Add(emp);
+            return Ok(employes);
+        }
+        [HttpPut]
+        public async Task<ActionResult<List<employe>>> UpdateEmployee(employe request)
+        {
+            var employe = employes.Find(e => e.id == request.id);
+            if (employe == null)
+            {
+                return BadRequest("employee not found");
+            }
+            employe.name = request.name;
+            employe.FirstName = request.FirstName;
+            employe.LastName = request.LastName;
+            employe.place=request.place;    
+            return Ok(employes);
+        }
+        [HttpDelete("{id}")]
+
+        public async Task<ActionResult<List<employe>>> Delete(int id)
+        {
+            var employe = employes.Find(e => e.id == id);
+            if (employe == null)
+            {
+                return BadRequest("employee not found");
+            }
+            employes.Remove(employe);
             return Ok(employes);
         }
     }
